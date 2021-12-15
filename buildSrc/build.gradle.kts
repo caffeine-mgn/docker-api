419fbbd89f8200c8c0600e522301d680ef6e1edd
subochev <caffeine.mgn@gmail.com>
2021-12-15 21:32:51 +0300

    Refactoring

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,12 +6,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
 
 plugins{
-        kotlin("jvm") version "1.6.0"
+        kotlin("jvm") version "1.6.10"
 }
 
 repositories {
@@ -20,9 +20,9 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.6.0")
 }
