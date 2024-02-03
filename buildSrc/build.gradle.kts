75e5a272ecceb9daba83a233a0a9ef0751b83b9a
subochev <caffeine.mgn@gmail.com>
2024-02-03 03:12:23 +0300

    kotlin version update 1.7.10 -> 1.9.21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     api("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-    api("pw.binom:binom-publish:0.1.8")
+    api("pw.binom:binom-publish:0.1.13")
 }
