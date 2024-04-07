fcc7b7dbba5ac27fd6206f9267bcc6cb9c8d3b36
subochev <caffeine.mgn@gmail.com>
2024-04-07 12:04:37 +0300

    update publisher plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     api("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-    api("pw.binom:binom-publish:0.1.14")
+    api("pw.binom:binom-publish:0.1.19")
 }
